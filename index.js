4888f02be0ea6c15c8106cfdc532ba8d831fc30d
Reshad Sadik <reshadsadik@gmail.com>
2021-11-12 05:04:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,139 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+// const { initializeApp } = require('firebase-admin/app');
+// const admin = require('firebase-admin');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ffsd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('automotive');
+    const productsCollection = database.collection('products');
+    const ordersCollection = database.collection('orders');
+    const reviewsCollection = database.collection('reviews');
+    const usersCollection = database.collection('users');
+
+    // get all products
+    app.get('/products', async (req, res) => {
+      const cursor = productsCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    // get all orders
+    app.get('/orders', async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const result = await cursor.toArray();
+
+      res.json(result);
+    });
+    // get all reviews
+    app.get('/reviews', async (req, res) => {
+      const cursor = reviewsCollection.find({});
+      const result = await cursor.toArray();
+
+      res.json(result);
+    });
+
+    // add an user 'POST'
+    app.post('/users', async (req, res) => {
+      const data = req.body;
+      const result = await usersCollection.insertOne(data);
+
+      res.json(result);
+    });
+
+    // add an user 'PUT'
+    app.put('/users', async (req, res) => {
+      const data = req.body;
+      const filter = { email: data.email };
+      const options = { upsert: true };
+
+      const updateDoc = {
+        $set: {
+          email: data.email,
+          displayName: data.displayName,
+        },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+    // add an admin
+
+    app.put('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+      console.log(email);
+      const filter = { email: email };
+
+      const updateDoc = {
+        $set: {
+          role: 'admin',
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      // const result = await usersCollection.insertOne(data);
+
+      res.json(result);
+    });
+
+    // add an review data
+    app.post('/reviews', async (req, res) => {
+      const data = req.body;
+      const result = await reviewsCollection.insertOne(data);
+
+      res.json(result);
+    });
+
+    // place an order data
+    app.post('/order', async (req, res) => {
+      const data = req.body;
+      const result = await ordersCollection.insertOne(data);
+
+      res.json(result);
+    });
+
+    // remove a product
+    app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+    });
+  } catch {}
+}
+
+run().catch(console.dir);
+
+client.connect((err) => {
+  const collection = client.db('test').collection('devices');
+  // perform actions on the collection object
+  //   client.close();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
