24782410e547930b3759178f202198a3d07fc6ab
Reshad Sadik <reshadsadik@gmail.com>
2021-11-26 04:41:28 +0600

    ff

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const SSLCommerzPayment = require('sslcommerz');
+const { v4: uuidv4 } = require('uuid');
+
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 5000;
 
@@ -26,6 +31,124 @@ async function run() {
     const ordersCollection = database.collection('orders');
     const reviewsCollection = database.collection('reviews');
     const usersCollection = database.collection('users');
+
+    // Initialize payment
+    app.post('/init', async (req, res) => {
+      const productInfo = {
+        total_amount: 50000,
+        currency: 'BDT',
+        tran_id: uuidv4(),
+        success_url: 'http://localhost:5000/success',
+        fail_url: 'http://localhost:5000/failure',
+        cancel_url: 'http://localhost:5000/cancel',
+        ipn_url: 'http://localhost:5000/ipn',
+        paymentStatus: 'pending',
+        shipping_method: 'Courier',
+        product_name: req.body.product_name,
+        product_category: 'Electronic',
+        product_profile: 'anything',
+        product_image: 'good',
+        cus_name: req.body.cus_name,
+        cus_email: req.body.cus_email,
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: req.body.cus_name,
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+        multi_card_name: 'mastercard',
+        value_a: 'ref001_A',
+        value_b: 'ref002_B',
+        value_c: 'ref003_C',
+        value_d: 'ref004_D',
+      };
+      // Insert order info
+      const result = await ordersCollection.insertOne(productInfo);
+
+      const sslcommer = new SSLCommerzPayment(
+        process.env.STORE_ID,
+        process.env.STORE_PASSWORD,
+        false
+      ); //true for live default false for sandbox
+      sslcommer.init(productInfo).then((data) => {
+        //process the response that got from sslcommerz
+        //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+        const info = { ...productInfo, ...data };
+        console.log(data);
+        if (info.GatewayPageURL) {
+          res.json(info.GatewayPageURL);
+        } else {
+          return res.status(400).json({
+            message: 'SSL session was not successful',
+          });
+        }
+      });
+    });
+
+    app.post('/success', async (req, res) => {
+      const result = await ordersCollection.updateOne(
+        { tran_id: req.body.tran_id },
+        {
+          $set: {
+            val_id: req.body.val_id,
+          },
+        }
+      );
+
+      res.redirect(`http://localhost:5000/success/${req.body.tran_id}`);
+    });
+    app.post('/failure', async (req, res) => {
+      const result = await ordersCollection.deleteOne({
+        tran_id: req.body.tran_id,
+      });
+
+      res.redirect(`http://localhost:5000`);
+    });
+    app.post('/cancel', async (req, res) => {
+      const result = await ordersCollection.deleteOne({
+        tran_id: req.body.tran_id,
+      });
+
+      res.redirect(`http://localhost:5000`);
+    });
+    app.post('/ipn', (req, res) => {
+      console.log(req.body);
+      res.send(req.body);
+    });
+    app.post('/validate', async (req, res) => {
+      const result = await ordersCollection.findOne({
+        tran_id: req.body.tran_id,
+      });
+
+      if (result.val_id === req.body.val_id) {
+        const update = await ordersCollection.updateOne(
+          { tran_id: req.body.tran_id },
+          {
+            $set: {
+              paymentStatus: 'paymentComplete',
+            },
+          }
+        );
+        console.log(update);
+        res.send(update.modifiedCount > 0);
+      } else {
+        res.send('Chor detected');
+      }
+    });
+    app.get('/orders/:tran_id', async (req, res) => {
+      const id = req.params.tran_id;
+      const result = await ordersCollection.findOne({ tran_id: id });
+      res.json(result);
+    });
 
     // get all products
     app.get('/products', async (req, res) => {
@@ -162,12 +285,25 @@ async function run() {
       const result = await ordersCollection.deleteOne(query);
       res.json(result);
     });
+
     // remove an product
     app.delete('/products/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await productsCollection.deleteOne(query);
       res.json(result);
+    });
+
+    // stripe payment gateway
+    app.post('/create-payment-intent', async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = paymentInfo.price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: 'usd',
+        amount: amount,
+        payment_method_types: ['card'],
+      });
+      res.json({ clientSecret: paymentIntent.client_secret });
     });
   } catch {}
 }
